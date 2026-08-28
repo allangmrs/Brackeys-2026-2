e1f0d6852bd8594befdd2a8731fc9ddcfbf3439c
allangmrs <allan81.guima@gmail.com>
2026-08-28 14:34:28 -0300

    [ADD] Fairy Dialogue Camera Zoom and player inputs disabled

--- Assets/Fairy/FairyDialogueTrigger.cs
+++ Assets/Fairy/FairyDialogueTrigger.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using DialogueSystem;
+using Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Fairy
 {
@@ -11,7 +13,9 @@ namespace Fairy
         [SerializeField] private DialogueData dialogue;
         [SerializeField] private FairyDialoguePoint talkingPoint;
         [SerializeField] private bool triggerOnce = true;
-        
+
+        private InputHandler playerInput;
+        private DialogueCameraController cameraController;
         private Collider2D triggerCollider;
         private bool hasTriggered = false;
         private bool isRunning = false;
@@ -19,6 +23,7 @@ namespace Fairy
         private void Awake()
         {
             triggerCollider = GetComponent<Collider2D>();
+            cameraController = FindAnyObjectByType<DialogueCameraController>();
         }
 
         public void OnTriggerEnter2D(Collider2D collision)
@@ -37,6 +42,11 @@ namespace Fairy
                 hasTriggered = true;
                 triggerCollider.enabled = false;
             }
+            
+            playerInput = collision.transform.root.GetComponentInChildren<InputHandler>();
+            playerInput.DisableInputs();
+
+            cameraController.FocusBetween(collision.transform, fairyController.transform);
 
             isRunning = true;
             StartCoroutine(StartDialogue());
@@ -51,6 +61,7 @@ namespace Fairy
                 () => {
                     fairyController.ResumeFollowing();
                     isRunning = false;
+                    playerInput.EnableInputs();
                 });
         }
     }

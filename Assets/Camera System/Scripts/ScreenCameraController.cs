98b98731c9cef3fceb87cdee6ef30e3a6495d120
allangmrs <allan81.guima@gmail.com>
2026-08-29 15:52:34 -0300

    [ADD] Camera Screen System

---
+++ Assets/Camera System/Scripts/ScreenCameraController.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace CameraSystem
+{
+    public class ScreenCameraController : MonoBehaviour
+    {
+        public static ScreenCameraController Instance { get; private set; }
+
+        [Header("Screen Transition Settings")]
+        [SerializeField] private Ease transitionEase = Ease.InOutQuad;
+        [SerializeField] private float transitionTime = 0.5f;
+        [SerializeField] private CinemachineCamera currentCamera;
+        [SerializeField] private Transform followPoint;
+
+        private Tween transitionTween;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            followPoint = currentCamera.Follow;
+        }
+
+        public void SetScreen(Transform cameraPoint)
+        {
+
+            if (cameraPoint == null || followPoint == null)
+            {
+                return;
+            }
+
+            transitionTween?.Kill();
+
+            transitionTween = followPoint
+                .DOMove(cameraPoint.position, transitionTime)
+                .SetEase(transitionEase);
+        }
+    }
+}
---
+++ Assets/Camera System/Scripts/ScreenCameraZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CameraSystem
+{
+    public class ScreenCameraZone : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraPoint;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                ScreenCameraController.Instance.SetScreen(cameraPoint);
+            }
+        }
+    }
+}
